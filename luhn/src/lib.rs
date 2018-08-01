18c62961618c27bef72c63220a257b0d0ad3e2d7
Björn Marschollek <git@muffix.de>
2018-08-01 18:39:48 +0100

    🎨 Refactor Luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,19 +1,20 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let parsed = validate_input(code);
+    let filtered = code.chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>();
 
-    match parsed {
-        Some(input) => {
-            input
-                .chars()
-                .rev()
-                .filter_map(|c| c.to_digit(10))
-                .enumerate()
-                .map(|(i, d)| if i % 2 == 1 { luhn_double(d) } else { d })
-                .sum::<u32>() % 10 == 0
-        }
-        None => false,
+    if filtered.chars().any(|c| !c.is_digit(10)) || filtered.len() < 2 {
+        return false;
     }
+
+    filtered
+        .chars()
+        .rev()
+        .filter_map(|c| c.to_digit(10))
+        .enumerate()
+        .map(|(i, d)| if i % 2 == 1 { luhn_double(d) } else { d })
+        .sum::<u32>() % 10 == 0
 }
 
 fn luhn_double(d: u32) -> u32 {
@@ -22,13 +23,4 @@ fn luhn_double(d: u32) -> u32 {
     } else {
         d * 2
     }
-}
-
-fn validate_input(code: &str) -> Option<String> {
-    let cleaned = code.replace(" ", "");
-
-    if cleaned.parse::<i32>().is_ok() && cleaned.len() > 1 {
-        return Some(cleaned);
-    }
-    None
 }
