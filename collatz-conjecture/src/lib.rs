2e6616131f3d7925b7b8e768810cf8165c13ef02
Björn Marschollek <git@muffix.de>
2018-08-21 10:57:46 +0100

    🎨 Refactor Collatz Conjecture to use a custom iterator

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,18 +1,30 @@
-pub fn collatz(mut n: u64) -> Option<u64> {
+struct Collatz {
+    curr: u64,
+}
+
+impl Iterator for Collatz {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        let res = self.curr;
+        self.curr = match self.curr {
+            n if n == 1 => 1,
+            n if n % 2 == 0 => n / 2,
+            n => n * 3 + 1,
+        };
+        Some(res)
+    }
+}
+
+impl Collatz {
+    pub fn new(start: u64) -> Collatz {
+        Collatz { curr: start }
+    }
+}
+
+pub fn collatz(n: u64) -> Option<u64> {
     if n == 0 {
         return None;
     }
-
-    let mut steps = 0;
-
-    while n != 1 {
-        if n % 2 == 0 {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
-        }
-        steps += 1
-    }
-
-    Some(steps)
+    Some(Collatz::new(n).take_while(|&n| n != 1).count() as u64)
 }
